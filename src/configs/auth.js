79db5a71348683ac5d3744e3f44be94466068013
Leonardo <leo.santosp@outlook.com>
2024-05-20 17:35:19 -0300

    upload 20-05

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -2,6 +2,5 @@ module.exports = {
     jwt: {
         secret: "default", // Colocar uma palavra ou hash para gerar o token
         expiresIn: "1d" // tempo de expiração 1d -> 1 dia
-        
     }
 }
