55b623c04faa7f8e5f4d0ec591b815c219b87407
Leonardo Santos <leo.santosp@outlook.com>
2024-06-12 22:23:50 -0300

    Finalizado o teste para criação de usuário

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,7 +1,8 @@
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
 const UserCreateService = require('./UserCreateService');
 
 // O it é uma função que recebe dois parametros (descrição do teste, funcao que vai executar o teste)
-it("user should be create", () => {
+it("user should be create", async () => {
     // Primeiro teste é verificar se o usuário é criado com sucesso
     const user = {
         name: "User Test",
@@ -10,8 +11,11 @@ it("user should be create", () => {
     };
     
     //Executar testes de forma independente do banco de dados
-    const userCreateService = new UserCreateService();
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreated = await userCreateService.execute(user);
+
+    console.log(userCreated);
 
     expect(userCreated).toHaveProperty("id");
 
